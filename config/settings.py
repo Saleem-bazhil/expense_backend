54ba2eba18c733e2ed84fa9fbec4cd2b0a2d4242
vaseem148 <mohamedvaseem114@gmail.com>
2026-05-08 19:04:57 +0530

    fix: allow all origins by default for CORS

--- config/settings.py
+++ config/settings.py
@@ -177,7 +177,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ---------------------------------------------------------------------------
 # CORS
 # ---------------------------------------------------------------------------
-CORS_ALLOW_ALL_ORIGINS = env_bool('CORS_ALLOW_ALL_ORIGINS', default=False)
+CORS_ALLOW_ALL_ORIGINS = env_bool('CORS_ALLOW_ALL_ORIGINS', default=True)
 DEFAULT_CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
